f36d0d628966fa361495b8cb2153db2e5879a00b
Benny233 <1026658608@qq.com>
2018-06-14 18:00:19 +0800

    Site updated: 2018-06-14 18:00:18

---
+++ js/report.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", function (event) {
+    let urlObj = {
+        method: 'get',
+        url: 'localhost:3888',
+        params: {
+            time: getTime(),
+            ipinfo: getIpinfo(),
+            url: getUrl(),
+            refer: getRefer(),
+            agent: getAgent(),
+            cookie: getCookie(),
+        },
+    };
+    axios(urlObj);
+});
+
+function getTime() {
+    let nowDate = new Date();
+    return nowDate.toLocaleDateString();
+}
+async function getIpinfo() {
+    let info = await axios({
+        method: 'get',
+        url: 'http://ip-api.com/json',
+    }).then((result) => {
+        return result.data; 
+    }).catch((err) => {
+        console.log('请求ip信息出错');
+        console.log(err);  
+    });
+    return info;
+}
+function getUrl() {
+    return window.location.href
+}
+function getRefer() {
+    return document.referrer;
+}
+function getAgent() {
+    return navigator.userAgent;
+}
+function getCookie() {
+    return document.cookie;
+}
